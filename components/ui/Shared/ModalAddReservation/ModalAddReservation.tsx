6bb6c837a399e807e2922b9950a59a1e699564b3
juanjosebl2 <juanjosebl2@gmail.com>
2024-08-04 19:37:34 +0200

    finich list cars and selec modal reservation card

--- components/ui/Shared/ModalAddReservation/ModalAddReservation.tsx
+++ components/ui/Shared/ModalAddReservation/ModalAddReservation.tsx
@@ -1,10 +1,60 @@
+import { Car } from "@prisma/client";
+import { Button } from "@/components/ui/button";
 import { ModalAddReservationProps } from "./ModalAddReservation.types";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { CalendarSelector } from "./CalendarSelector";
+import { useState } from "react";
+import { addDays } from "date-fns";
+import { DateRange } from "react-day-picker";
 
-export function ModalAddReservation( props: ModalAddReservationProps ) {
-  
-    const { car } = props;
-    
-return (
-    <div>Añadir modelo de reserva</div>
-  )
+export function ModalAddReservation(props: ModalAddReservationProps) {
+  const { car } = props;
+
+  const [dateSelected, setDateSelected] = useState<{
+    from: Date | undefined;
+    to: Date | undefined;
+  }>({
+    from: new Date(),
+    to: addDays(new Date(), 5),
+  });
+
+  const onReserveCar = async (car: Car, dateSelected: DateRange) => {
+    console.log("Reservar coche");
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" className="w-full mt-3">
+          Realizar reserva
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Seleccionar fechar para alquilar coche
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            <CalendarSelector setDateSelected={setDateSelected} carPriceDay = {car.priceDay}/>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={() => onReserveCar(car, dateSelected)}>
+            Reservar vehiculo
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
